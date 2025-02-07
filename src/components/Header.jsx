1b9a03eee9e35a5f67d83a644be85a36469f9251
Abolfazl <iabolfazl83@yahoo.com>
2025-02-08 01:45:33 +0330

    updated Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,32 @@
 import logo from '../assets/logo.jpg'
-import {useContext} from "react";
-import {appContext} from "../context/AppContext.jsx";
+import {Cart} from "./Cart.jsx";
+import {useState} from "react";
 
 export function Header() {
-  const {setOpen} = useContext(appContext);
+  const [visible, setVisible] = useState(false);
 
   return (
-    <header id="main-header">
-      <div id="title">
-        <img src={logo} alt="resurant logo"/>
-        <h1>
-          REACTFOOD
-        </h1>
-      </div>
-      <nav>
-        <button className="text-button" onClick={() => {setOpen(true)}}>Cart (0)</button>
-      </nav>
-    </header>
+    <>
+      <header id="main-header">
+        <div id="title">
+          <img src={logo} alt="resurant logo"/>
+          <h1>
+            REACTFOOD
+          </h1>
+        </div>
+        <nav>
+          <button className="text-button" onClick={() => {
+            setVisible(true)
+          }}>Cart (0)
+          </button>
+        </nav>
+      </header>
+      <Cart visible={visible} onClose={() => setVisible(false)}/>
+
+      {/*after submitting cart this below component should show*/}
+      {/*<Checkout open={openCart}/>*/}
+      {/*after submitting form component this below component should show*/}
+      {/*<checkoutResult open={openCart}/>*/}
+    </>
   )
 }
