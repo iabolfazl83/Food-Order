8d5813a4c7d109e2e5235c074f24292187ba7876
Abolfazl <iabolfazl83@yahoo.com>
2025-02-11 00:55:34 +0330

    created AppContext.jsx.

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -3,6 +3,10 @@ import {createContext, useContext, useState} from "react";
 export const appContext = createContext({
   cartItems: [],
   setCartItems: () => {},
+  addToCart: () => {},
+  removeFromCart: () => {},
+  clearCart: () => {},
+  getCartTotal: () => {},
 });
 
 export function useAppContext() {
@@ -10,10 +14,55 @@ export function useAppContext() {
 }
 
 const AppContextProvider = ({children}) => {
-  const [cartItems, setCartItems] = useState([])
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = (item) => {
+    const isItemInCart = cartItems.find(cartItem => cartItem.id === item.id);
+
+    if (isItemInCart) {
+      setCartItems(
+        cartItems.map(cartItem =>
+          cartItem.id === item.id
+            ? {...cartItem, quantity: cartItem.quantity + 1}
+            : cartItem
+        )
+      );
+    } else {
+      setCartItems([...cartItems, {...item, quantity: 1}]);
+    }
+  };
+
+  const removeFromCart = (item) => {
+    const isItemInCart = cartItems.find((cartItem) => cartItem.id === item.id);
+
+    if (isItemInCart.quantity === 1) {
+      setCartItems(cartItems.filter((cartItem) => cartItem.id !== item.id));
+    } else {
+      setCartItems(
+        cartItems.map((cartItem) =>
+          cartItem.id === item.id
+            ? {...cartItem, quantity: cartItem.quantity - 1}
+            : cartItem
+        )
+      );
+    }
+  };
+
+  const clearCart = () => {
+    setCartItems([]);
+  };
+
+  const getCartTotal = () => {
+    return cartItems.reduce((total, item) => (total + item.price * item.quantity), 0).toFixed(2);
+  };
+
   const appContextValues = {
     cartItems,
     setCartItems,
+    addToCart,
+    removeFromCart,
+    clearCart,
+    getCartTotal,
   }
 
   return (
