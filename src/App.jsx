0e1dceda3fec9a1db8840550e51cd16621ddeee8
Abolfazl <iabolfazl83@yahoo.com>
2025-02-11 00:55:08 +0330

    added AppContext Provider.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import {Header} from "./components/Header.jsx";
 import Meals from "./components/Meals.jsx";
+import AppContextProvider from "./context/AppContext.jsx";
 
 function App() {
   return (
-    <>
+    <AppContextProvider>
       <Header/>
       <Meals/>
-    </>
+    </AppContextProvider>
   );
 }
 
