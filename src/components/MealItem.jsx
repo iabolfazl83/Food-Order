3474b863737491c450c20a2b57442e6a31d60642
Abolfazl <iabolfazl83@yahoo.com>
2025-02-11 00:58:14 +0330

    created MealItem.jsx.

---
+++ src/components/MealItem.jsx
@@ -0,0 +1,28 @@
+import {currencyFormatter} from "../formatting.js";
+import Button from "./UI/Button.jsx";
+import {useAppContext} from "../context/AppContext.jsx";
+
+export default function MealItem({meal}) {
+  const {addToCart} = useAppContext();
+
+  return (
+    <div key={meal.id} className="meal-item">
+      <article>
+        <img src={`http://localhost:3000/${meal.image}`} alt={meal.name}/>
+        <div className="article">
+          <h3>{meal.name}</h3>
+          <p className="meal-item-price">${currencyFormatter.format(meal.price)}</p>
+          <p className="meal-item-description">
+            {meal.description}
+          </p>
+          <p className="meal-item-actions">
+            <Button onClick={() => {
+              addToCart(meal)
+            }}>Add to Cart
+            </Button>
+          </p>
+        </div>
+      </article>
+    </div>
+  )
+}
