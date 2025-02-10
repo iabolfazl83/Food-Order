a16fcd4d2e765c57da7146894621712bc23f6b76
Abolfazl <iabolfazl83@yahoo.com>
2025-02-11 00:56:16 +0330

    refactored Cart.jsx.

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,51 +1,10 @@
 import Dialog from "./Dialog.jsx";
 import {useAppContext} from "../context/AppContext.jsx";
+import {currencyFormatter} from "../formatting.js";
+import Button from "./UI/Button.jsx";
 
-export function Cart({visible, onClose}) {
-  const {cartItems, setCartItems} = useAppContext();
-
-  const addToCart = (item) => {
-    const isItemInCart = cartItems.find(cartItem => cartItem.id === item.id);
-
-    if (isItemInCart) {
-      setCartItems(
-        cartItems.map(cartItem =>
-          cartItem.id === item.id
-            ? {...cartItem, quantity: cartItem.quantity + 1}
-            : cartItem
-        )
-      );
-    } else {
-      setCartItems([...cartItems, {...item, quantity: 1}]);
-    }
-  };
-
-
-  const removeFromCart = (item) => {
-    const isItemInCart = cartItems.find((cartItem) => cartItem.id === item.id);
-
-    if (isItemInCart.quantity === 1) {
-      setCartItems(cartItems.filter((cartItem) => cartItem.id !== item.id));
-    } else {
-      setCartItems(
-        cartItems.map((cartItem) =>
-          cartItem.id === item.id
-            ? {...cartItem, quantity: cartItem.quantity - 1}
-            : cartItem
-        )
-      );
-    }
-  };
-
-  const clearCart = () => {
-    setCartItems([]);
-  };
-
-  const getCartTotal = () => {
-    return cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
-  };
-
-
+export function Cart({visible, onSubmit, onClose}) {
+  const {cartItems, getCartTotal, removeFromCart, addToCart} = useAppContext();
   return (
     <Dialog visible={visible}>
       {
@@ -53,15 +12,33 @@ export function Cart({visible, onClose}) {
           <div className="cart">
             <h2>Your Cart</h2>
             <ul>
-              <li className="cart-item"></li>
+              {
+                cartItems.map(item => (
+                  <li key={item.id} className="cart-item">
+                    {item.name} - {item.quantity}x ${currencyFormatter.format(item.price)}
+                    <div className="cart-item-actions">
+                      <button onClick={() => {
+                        removeFromCart(item)
+                      }}>-
+                      </button>
+                      <p>{item.quantity}</p>
+                      <button onClick={() => {
+                        addToCart(item)
+                      }}>+
+                      </button>
+                    </div>
+                  </li>
+                ))
+              }
             </ul>
+            <div className="cart-total">${getCartTotal()}</div>
             <div className="modal-actions">
-              <button className="text-button" onClick={() => {
+              <Button textOnly onClick={() => {
                 close();
                 onClose();
               }}>Close
-              </button>
-              <button className="button">Go to Checkout</button>
+              </Button>
+              <Button onClick={onSubmit}>Go to Checkout</Button>
             </div>
           </div>
         )
