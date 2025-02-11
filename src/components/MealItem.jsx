a0f6f163bfc14f5be9210296236228b6eef52d1c
AbolfazlAbbaspour <a.abaspor@gmail.com>
2025-02-11 16:45:40 +0330

    fixed typo.

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -11,7 +11,7 @@ export default function MealItem({meal}) {
         <img src={`http://localhost:3000/${meal.image}`} alt={meal.name}/>
         <div className="article">
           <h3>{meal.name}</h3>
-          <p className="meal-item-price">${currencyFormatter.format(meal.price)}</p>
+          <p className="meal-item-price">{currencyFormatter.format(meal.price)}</p>
           <p className="meal-item-description">
             {meal.description}
           </p>
