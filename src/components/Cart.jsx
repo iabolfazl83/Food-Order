4093b4d49d0be5bea273c6f76f6120408b14cfaa
Abolfazl <iabolfazl83@yahoo.com>
2025-02-16 00:02:55 +0330

    fixed closing Modal with esc key. added check for cart items in popup actions.

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -6,36 +6,39 @@ import Button from "./UI/Button.jsx";
 export function Cart({visible, onSubmit, onClose}) {
   const {cartItems, getCartTotal, removeFromCart, addToCart} = useAppContext();
   return (
-    <Modal visible={visible} className="cart">
-        <h2>Your Cart</h2>
-        <ul>
-          {
-            cartItems.map(item => (
-              <li key={item.id} className="cart-item">
-                {item.name} - {item.quantity}x ${currencyFormatter.format(item.price)}
-                <div className="cart-item-actions">
-                  <button onClick={() => {
-                    removeFromCart(item)
-                  }}>-
-                  </button>
-                  <p>{item.quantity}</p>
-                  <button onClick={() => {
-                    addToCart(item)
-                  }}>+
-                  </button>
-                </div>
-              </li>
-            ))
-          }
-        </ul>
-        <div className="cart-total">${getCartTotal()}</div>
-        <div className="modal-actions">
-          <Button textOnly onClick={() => {
-            onClose();
-          }}>Close
-          </Button>
+    <Modal visible={visible} className="cart" onClose={onClose}>
+      <h2>Your Cart</h2>
+      <ul>
+        {
+          cartItems.map(item => (
+            <li key={item.id} className="cart-item">
+              {item.name} - {item.quantity}x {currencyFormatter.format(item.price)}
+              <div className="cart-item-actions">
+                <button onClick={() => {
+                  removeFromCart(item)
+                }}>-
+                </button>
+                <p>{item.quantity}</p>
+                <button onClick={() => {
+                  addToCart(item)
+                }}>+
+                </button>
+              </div>
+            </li>
+          ))
+        }
+      </ul>
+      <div className="cart-total">${getCartTotal()}</div>
+      <div className="modal-actions">
+        <Button textOnly onClick={() => {
+          onClose();
+        }}>Close
+        </Button>
+        {
+          cartItems.length > 0 &&
           <Button onClick={onSubmit}>Go to Checkout</Button>
-        </div>
+        }
+      </div>
     </Modal>
   );
 }
