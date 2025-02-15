1a393d7d6796868fe43c9293ec2749b70a4c63cd
Abolfazl <iabolfazl83@yahoo.com>
2025-02-16 00:09:06 +0330

    removed http.js.

--- src/http.js
+++
@@ -1,9 +0,0 @@
-export async function fetchMeals() {
-  const meals = await fetch("http://localhost:3000/meals");
-  const resData = await meals.json();
-
-  if (!meals.ok) {
-    throw new Error('Failed to fetch meals.');
-  }
-  return resData;
-}
